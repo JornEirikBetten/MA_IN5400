b5136cfe962a59ee26f723611b80680b970a8158
JornEirikBetten <jorneirikbetten1@gmail.com>
2022-03-16 21:43:51 +0100

    Kaiming-He

--- train_pytorch_in5400_studentversion.py
+++ train_pytorch_in5400_studentversion.py
@@ -316,7 +316,7 @@ class yourloss(nn.modules.loss._Loss):
         return loss
 
 
-def runstuff():
+def runstuff(): # Single net, 3 channels
   config = dict()
   config['use_gpu'] = True #True #TODO change this to True for training on the cluster
   config['lr'] = 0.01
@@ -457,7 +457,7 @@ def runstuff():
   file_lbls.close()
   file_fnames.close()
 
-def runstuff3():
+def runstuff3(): # Two nets, 4 channels kaiminghe
   config = dict()
   config['use_gpu'] = True #True #TODO change this to True for training on the cluster
   config['lr'] = 0.01
@@ -601,10 +601,150 @@ def runstuff3():
   file_lbls.close()
   file_fnames.close()
 
+def runstuff4(): # Single net, 4 channels kaiming-he
+  config = dict()
+  config['use_gpu'] = True #True #TODO change this to True for training on the cluster
+  config['lr'] = 0.01
+  config['batchsize_train'] = 16
+  config['batchsize_val'] = 34
+  config['maxnumepochs'] = 1
+  config['scheduler_stepsize'] = 2
+  config['scheduler_factor'] = 0.3
 
+  # This is a dataset property.
+  config['numcl'] = 17
+  seed = 20001
+  if config['use_gpu'] == True:
+      torch.cuda.manual_seed_all(seed)
+  else:
+      torch.manual_seed_all(seed)
+
+
+
+  # Data augmentations.
+  data_transforms = {
+      'train': transforms.Compose([
+          transforms.Resize(256),
+          transforms.RandomCrop(224),
+          transforms.RandomHorizontalFlip(),
+          transforms.ToTensor(),
+          ChannelSelect(channels=[0, 1, 2, 3]),
+          transforms.Normalize([0.7476, 0.6534, 0.4757, 0.0960], [0.1677, 0.1828, 0.2137, 0.0284])
+      ]),
+      'val': transforms.Compose([
+          transforms.Resize(224),
+          transforms.CenterCrop(224),
+          transforms.RandomHorizontalFlip(),
+          transforms.ToTensor(),
+          ChannelSelect(channels=[0, 1, 2, 3]),
+          transforms.Normalize([0.7476, 0.6534, 0.4757, 0.0960], [0.1677, 0.1828, 0.2137, 0.0284])
+      ]),
+  }
+
+  dirname = os.getcwd()
+  main_path = dirname + "/rainforest"
+  gpu_path = "/itf-fi-ml/shared/IN5400/2022_mandatory1/"
+  print(main_path)
+
+
+  # Device
+  if True == config['use_gpu']:
+      device= torch.device('cuda:0')
+      path = gpu_path
+  else:
+      device= torch.device('cpu')
+      path = main_path
+  # Datasets
+  image_datasets={}
+  #image_datasets['train']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/',trvaltest=0, transform=data_transforms['train'])
+  #image_datasets['val']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/',trvaltest=1, transform=data_transforms['val'])
+  image_datasets['train']=RainforestDataset(root_dir=path,trvaltest=0, transform=data_transforms['train'])
+  image_datasets['val']=RainforestDataset(root_dir=path,trvaltest=1, transform=data_transforms['val'])
+  # Dataloaders
+  #TODO use num_workers=1
+  dataloaders = {}
+  dataloaders['train'] = DataLoader(image_datasets['train'], batch_size=config['batchsize_train'], shuffle=True)
+  dataloaders['val'] = DataLoader(image_datasets['val'], batch_size=config['batchsize_val'], shuffle=False)
+
+  # Model
+  # TODO create an instance of the network that you want to use.
+  pretrained_net = resnet18(pretrained=True)# TwoNetworks()
+  model = SingleNetwork(pretrained_net, weight_init='kaiming-he')
+
+  model = model.to(device)
+
+
+  lossfct = yourloss()
+
+  #TODO
+  # Observe that all parameters are being optimized
+  someoptimizer = torch.optim.Adam(params = model.parameters(), lr = config['lr'], betas = (0.9, 0.999))
+
+  # Decay LR by a factor of 0.3 every X epochs
+  #TODO
+  somelr_scheduler = torch.optim.lr_scheduler.StepLR(someoptimizer, step_size=config['scheduler_stepsize'], gamma=config['scheduler_factor'])
+
+  best_epoch, best_measure, bestweights, trainlosses, testlosses, testperfs, trainperfs, preds, labels, fnames = traineval2_model_nocv(dataloaders['train'], dataloaders['val'] ,  model ,  lossfct, someoptimizer, somelr_scheduler, num_epochs= config['maxnumepochs'], device = device , numcl = config['numcl'] )
+  print("Best epoch: ", best_epoch)
+  print("Best measure: ", best_measure)
+  # Save best weights
+  torch.save(bestweights, "./best_model.pt")
+  # Write important outputs to file
+  file_classes = open('classes_avg_scores.csv', 'w')
+  class_writer = csv.writer(file_classes)
+  header_row_classes = ['clear', 'cloudy', 'haze', 'partly_cloudy',
+             'agriculture', 'artisinal_mine', 'bare_ground', 'blooming',
+             'blow_down', 'conventional_mine', 'cultivation', 'habitation',
+             'primary', 'road', 'selective_logging', 'slash_burn', 'water']
+  class_writer.writerow(header_row_classes)
+  file_losses = open('losses_mAP.csv', 'w')
+  loss_writer = csv.writer(file_losses)
+  header_row_losses = ['training_loss','validation_loss','mAP_test', 'mAP_train']
+  loss_writer.writerow(header_row_losses)
+
+  for epoch in range(config['maxnumepochs']):
+      test_performances_at_epoch = testperfs[epoch]
+      train_performances_at_epoch = trainperfs[epoch]
+      mAP_test = np.mean(test_performances_at_epoch)
+      mAP_train = np.mean(train_performances_at_epoch)
+      class_writer.writerow(test_performances_at_epoch)
+      performance_measures = []
+      performance_measures.append(trainlosses[epoch])
+      performance_measures.append(testlosses[epoch])
+      performance_measures.append(mAP_test)
+      performance_measures.append(mAP_train)
+      loss_writer.writerow(performance_measures)
+
+  file_classes.close()
+  file_losses.close()
+  file_fnames = open('fnames.csv', 'w')
+  name_writer = csv.writer(file_fnames)
+  name_writer.writerow(["filenames"])
+
+
+  file_pred = open('predictions.csv', 'w')
+  pred_writer = csv.writer(file_pred)
+  pred_writer.writerow(header_row_classes)
+
+  file_lbls = open('labels.csv', 'w')
+  lbls_writer = csv.writer(file_lbls)
+  lbls_writer.writerow(header_row_classes)
+
+  for i in range(preds.shape[0]):
+      pred_row = preds[i, :]
+      lbls_row = labels[i, :]
+      filename = fnames[i]
+      pred_writer.writerow(pred_row)
+      lbls_writer.writerow(lbls_row)
+      name_writer.writerow([filename])
+
+  file_pred.close()
+  file_lbls.close()
+  file_fnames.close()
 
 
 if __name__=='__main__':
 
-  #runstuff()
-  runstuff3()
+  #runstuff() # Single net, 3 channels
+  #runstuff3() # Two nets, 4 channels 
+  runstuff4() # Single net, 4 channels kaiming-he initialization
